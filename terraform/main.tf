e2201d7de0b3bef46cfb79f3f6641d0ff8839201
mystique4u <mystique4u@gmail.com>
2024-10-18 14:20:08 +0200

    add firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "itin"  # Replace with your Terraform Cloud organization name
+    organization = "itin"
 
     workspaces {
-      name = "hetznercloud"  # Replace with the Terraform Cloud workspace name
+      name = "hetznercloud"
     }
   }
 
@@ -24,12 +24,17 @@ data "hcloud_ssh_key" "existing" {
   name = var.ssh_key_name  # Use the name of the existing SSH key
 }
 
-# Hetzner Cloud VM resource
+# Data block to retrieve the existing firewall by name
+data "hcloud_firewall" "existing" {
+  name = var.firewall_name  # Firewall name to attach
+}
+
+# Hetzner Cloud VM resource with firewall
 resource "hcloud_server" "vm" {
   name        = "github-action-vm"
-  server_type = "cx22"  # Choose the server type (e.g., cx11, cx21, etc.)
-  image       = "ubuntu-22.04"  # Base OS image
-  location    = "nbg1"  # Hetzner data center location (e.g., nbg1, fsn1)
+  server_type = "cx22"
+  image       = "ubuntu-22.04"
+  location    = "nbg1"
 
   public_net {
     ipv4_enabled = true  # Enable IPv4
@@ -39,13 +44,15 @@ resource "hcloud_server" "vm" {
   ssh_keys = [
     data.hcloud_ssh_key.existing.name  # Use the existing SSH key
   ]
+
+  firewall_ids = [data.hcloud_firewall.existing.id]  # Attach the firewall by ID
 }
 
 # Output the public IPv4 address of the created VM
 output "public_ip" {
   description = "The public IPv4 address of the VM"
   value       = hcloud_server.vm.ipv4_address
-  sensitive   = true  # Sensitive output, useful for security
+  sensitive   = true
 }
 
 # Variables
@@ -62,5 +69,10 @@ variable "ssh_key_name" {
 
 variable "ssh_public_key" {
   description = "Public SSH key content to use for access"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "Name of the Hetzner firewall to apply"
   type        = string
 }
