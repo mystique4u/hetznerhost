6fba0e797fa9f0236638cb31c541483ac4f65d99
mystique4u <yurii.masalitin@gmail.com>
2024-12-26 17:38:19 +0100

    try to deploy second container

--- compose/apps/demo/app.py
+++ compose/apps/demo/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Python Web Application! APP!"
+    return "Welcome to the Python Web Application DEMO!"
 
 @app.route('/hello')
 def hello():
     return "Hello from Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
